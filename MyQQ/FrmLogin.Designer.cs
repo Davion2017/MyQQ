3ae09ac6edade7cfa116bd6f7c0b49f1626e87a2
Davion <davion2017@foxmail.com>
2019-05-30 11:51:51 +0800

    登录窗体界面设计完成

--- MyQQ/FrmLogin.Designer.cs
+++ MyQQ/FrmLogin.Designer.cs
@@ -28,13 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmLogin));
+            this.panel_title = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnMin = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.picHead = new System.Windows.Forms.PictureBox();
+            this.labTitle = new System.Windows.Forms.Label();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.panel_title.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picHead)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel_title
+            // 
+            this.panel_title.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.panel_title.Controls.Add(this.labTitle);
+            this.panel_title.Location = new System.Drawing.Point(0, 0);
+            this.panel_title.Name = "panel_title";
+            this.panel_title.Size = new System.Drawing.Size(380, 35);
+            this.panel_title.TabIndex = 0;
+            this.panel_title.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Panel_title_MouseDown);
+            this.panel_title.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Panel_title_MouseMove);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("黑体", 17F);
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(142, 174);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(238, 31);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("宋体", 16F);
+            this.txtPassword.Location = new System.Drawing.Point(142, 220);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(238, 32);
+            this.txtPassword.TabIndex = 2;
+            // 
+            // btnMin
+            // 
+            this.btnMin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnMin.FlatAppearance.BorderSize = 0;
+            this.btnMin.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnMin.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnMin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMin.Location = new System.Drawing.Point(380, 0);
+            this.btnMin.Name = "btnMin";
+            this.btnMin.Size = new System.Drawing.Size(40, 35);
+            this.btnMin.TabIndex = 3;
+            this.btnMin.UseVisualStyleBackColor = false;
+            this.btnMin.Click += new System.EventHandler(this.BtnMin_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.btnClose.FlatAppearance.BorderSize = 0;
+            this.btnClose.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.btnClose.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.btnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClose.Location = new System.Drawing.Point(420, 0);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(50, 35);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.UseVisualStyleBackColor = false;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnSubmit.BackgroundImage")));
+            this.btnSubmit.Location = new System.Drawing.Point(214, 278);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(70, 23);
+            this.btnSubmit.TabIndex = 5;
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // picHead
+            // 
+            this.picHead.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.picHead.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("picHead.BackgroundImage")));
+            this.picHead.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.picHead.Location = new System.Drawing.Point(25, 174);
+            this.picHead.Name = "picHead";
+            this.picHead.Size = new System.Drawing.Size(100, 90);
+            this.picHead.TabIndex = 7;
+            this.picHead.TabStop = false;
+            // 
+            // labTitle
+            // 
+            this.labTitle.AutoSize = true;
+            this.labTitle.Font = new System.Drawing.Font("黑体", 20F);
+            this.labTitle.ForeColor = System.Drawing.Color.Gainsboro;
+            this.labTitle.Location = new System.Drawing.Point(13, 4);
+            this.labTitle.Name = "labTitle";
+            this.labTitle.Size = new System.Drawing.Size(68, 27);
+            this.labTitle.TabIndex = 0;
+            this.labTitle.Text = "MyQQ";
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.BackColor = System.Drawing.Color.White;
+            this.btnRegister.FlatAppearance.BorderSize = 0;
+            this.btnRegister.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRegister.ForeColor = System.Drawing.Color.RoyalBlue;
+            this.btnRegister.Location = new System.Drawing.Point(386, 201);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnRegister.TabIndex = 8;
+            this.btnRegister.Text = "注册账号";
+            this.btnRegister.UseVisualStyleBackColor = false;
+            // 
+            // FrmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(470, 360);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.picHead);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnMin);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.panel_title);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "FrmLogin";
             this.Text = "Form1";
+            this.panel_title.ResumeLayout(false);
+            this.panel_title.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picHead)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel_title;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnMin;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.PictureBox picHead;
+        private System.Windows.Forms.Label labTitle;
+        private System.Windows.Forms.Button btnRegister;
     }
 }
 
--- MyQQ/FrmLogin.cs
+++ MyQQ/FrmLogin.cs
@@ -16,5 +16,34 @@ namespace MyQQ
         {
             InitializeComponent();
         }
+
+        private Point mousePoint = new Point();
+
+        private void Panel_title_MouseDown(object sender, MouseEventArgs e)
+        {
+            base.OnMouseDown(e);
+            this.mousePoint.X = e.X;
+            this.mousePoint.Y = e.Y;
+        }
+
+        private void Panel_title_MouseMove(object sender, MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if(e.Button == MouseButtons.Left)
+            {
+                this.Left = Control.MousePosition.X - mousePoint.X;
+                this.Top = Control.MousePosition.Y - mousePoint.Y;
+            }
+        }
+
+        private void BtnMin_Click(object sender, EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
     }
 }
