a7129988d2e11e0300e2a203dd3cb79867e404d8
Davion <davion2017@foxmail.com>
2019-06-07 01:23:41 +0800

    加入FrmBase

---
+++ MyQQ/FrmBase.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyQQ
+{
+    public partial class FrmBase : Form
+    {
+        public FrmBase()
+        {
+            InitializeComponent();
+            skinEngine1 = new Sunisoft.IrisSkin.SkinEngine(((System.ComponentModel.Component)(this)));
+            skinEngine1.SkinFile = Application.StartupPath + "/office2007.ssk";
+        }
+    }
+}
